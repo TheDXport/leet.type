c956b1a68f35e8b8ae4f4d49388417cd9f6a6f1c
TheDXport <therealdx10@gmail.com>
2025-02-06 00:04:41 -0500

    file name error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 
-import Header from "./components/Header";
+import Header from "./components/Head";
 import SelectorBar from "./components/SelectorBar";
 import TypingArea from "./components/TypingArea";
 import FadeSwitch from "./components/FadeSwitch";
